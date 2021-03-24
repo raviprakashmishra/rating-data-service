8a3799cc676a57c866228819e641ee0c49f2e752
rmishra <ravi.nitp.ece@gmail.com>
2021-03-23 18:17:20 -0700

    added starting service

---
+++ src/main/java/com/ravi/ratingdataservice/model/Rating.java
@@ -0,0 +1,29 @@
+package com.ravi.ratingdataservice.model;
+
+public class Rating {
+    private String movieId;
+    private int rating;
+
+    public Rating(String movieId, int rating) {
+        this.movieId = movieId;
+        this.rating = rating;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public Rating setMovieId(String movieId) {
+        this.movieId = movieId;
+        return this;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public Rating setRating(int rating) {
+        this.rating = rating;
+        return this;
+    }
+}
